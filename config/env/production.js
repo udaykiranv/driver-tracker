e079d6ba1ec100c76f2dccaafdb2a5125ff5a260
Uday Kiran Vemparala <udaykiran@Udays-MacBook-Pro.local>
2015-04-27 18:41:43 +0530

    updated

--- config/env/production.js
+++ config/env/production.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod',
-  /**
+db: 'mongodb://uday:uday@ds063809.mongolab.com:63809/heroku_app36265109',  
+/**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
    * See http://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html#mongoclient-connect-options
